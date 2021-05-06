64ccf779894e9771892d3c95f24d05915e129ffa
Anara Orazalina <orazalina.anara@inbox.ru>
2021-05-06 22:04:21 +0600

    one test added

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,5 +1,8 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selectors;
+import com.codeborne.selenide.WebDriverProvider;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -8,25 +11,40 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+import static org.openqa.selenium.Keys.CONTROL;
+import static org.openqa.selenium.Keys.DELETE;
 
 public class CardDeliveryTest {
+
+    private LocalDate plusDays(int n) {
+        LocalDate date = LocalDate.now();
+        date = date.plusDays(n);
+
+        return date;
+    }
+
 
     @Test
     void allCorrect() {
         open("http://localhost:9999");
-        //$("").click();
-        //$("").setValue("Казань");
-        //$("").setValue("31.10.2019");
-        //$("").setValue("Иван Петров");
-        //$("").setValue("+77777777777");
-        //$("").click();
-        //$("").click();
-
-
-
-
+        $("[data-test-id=city] input").setValue("Казань");
+        $("[placeholder='Дата встречи']").sendKeys(CONTROL + "a", DELETE);
+        String newDate = plusDays(4).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        $("[placeholder='Дата встречи']").setValue(newDate);
+        $("[data-test-id=name] input").setValue("Иван Петров");
+        $("[data-test-id=phone] input").setValue("+77777777777");
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $(".button").click();
+        $(Selectors.withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(10));
     }
 }
