f5a6011f99a9e66caaf1460c23d3f3f600cdf6e2
Anara Orazalina <orazalina.anara@inbox.ru>
2021-05-13 01:14:44 +0600

    task2.3

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -2,39 +2,34 @@ package ru.netology;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selectors;
-import com.codeborne.selenide.WebDriverProvider;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.github.javafaker.Faker;
 import org.jetbrains.annotations.NotNull;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.remote.DesiredCapabilities;
-
-import java.io.File;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.Locale;
 
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.openqa.selenium.Keys.CONTROL;
-import static org.openqa.selenium.Keys.DELETE;
+
 
 public class CardDeliveryTest {
+    private Faker faker;
 
     @BeforeAll
     static void setupAll() {
         Configuration.browser = "firefox";
+    }
+
+    @BeforeEach
+    void setUpAll() {
+        faker = new Faker(new Locale("ru"));
+
     }
 
     @NotNull
@@ -59,5 +54,17 @@ public class CardDeliveryTest {
         $(".button").click();
         $("[data-test-id=notification]").shouldBe(Condition.visible, Duration.ofSeconds(15)).shouldHave(Condition.exactText("Успешно! Встреча успешно забронирована на " + when(false)));
 
+    }
+
+    @Test
+    void shouldAddFalseCity() {
+        open("http://localhost:9999/");
+        $("[data-test-id=city] input").setValue("К");
+        $("[data-test-id=date] [placeholder='Дата встречи']").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE, when(false));
+        $("[data-test-id=name] input").setValue("Иван Петров");
+        $("[data-test-id=phone] input").setValue("+77777777777");
+        $("[data-test-id=agreement] .checkbox__box").click();
+        $(".button").click();
+        $("[data-test-id=city] .input__sub").shouldBe(Condition.visible, Duration.ofSeconds(15)).shouldHave(Condition.exactText("Доставка в выбранный город недоступна"));
     }
 }
